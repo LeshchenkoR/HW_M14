e66be27df21adf9dfbb8554fbf35ebecbecf21a6
LeshchenkoR <Littleleshch@yandex.ru>
2022-02-21 21:36:53 +0300

    first commit

---
+++ Demo.java
@@ -0,0 +1,32 @@
+package HW_M14;
+
+class Demo {
+    public synchronized static void show() {
+        Thread tread = Thread.currentThread();
+        for (int i = 1; i <= 3; i++) {
+            System.out.println(tread.getName() + "[" + i + "]");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException exc) {
+            }
+        }
+    }
+
+    static class MyThread extends Thread {
+        MyThread(String name) {
+            super(name);
+            start();
+        }
+
+        @Override
+        public void run() {
+            Demo.show();
+        }
+
+        public static void main(String[] args) {
+            new MyThread("Alpha");
+            new MyThread("Bravo");
+            new MyThread("Foxtrot");
+        }
+    }
+}
